7e0501ba11c6065b231c9f276b8cc5b52340dbdb
magic3007 <maijing3007@qq.com>
2020-04-26 00:39:37 +0800

    [utils/bas_utils] update

--- project1/solutions/utils/base_utils.h
+++ project1/solutions/utils/base_utils.h
@@ -18,13 +18,32 @@ using std::unique_ptr;
 #define DEFER_CONCAT_IMPL(x, y) x##y
 #define DEFER_CONCAT(x, y)   DEFER_CONCAT_IMPL(x, y)
 
-class DeferAction {
+class DeferredAction {
   public:
-    DISALLOW_COPY_AND_ASSIGN(DeferAction);
-    explicit DeferAction(std::function<void(void)> f): f_(std::move(f)) {}
-    ~DeferAction(){f_();}
+    DISALLOW_COPY_AND_ASSIGN(DeferredAction);
+    explicit DeferredAction(std::function<void(void)> f): f_(std::move(f)) {}
+    ~DeferredAction(){f_();}
   private:
     std::function<void(void)> f_;
 };
+
+#define DEFER(action) \
+  DeferredAction DEFER_CONCAT(__deferred__, __LINE__) \
+  ([&]() -> void action)
+
+#define DCHECK(cond) \
+  if (!(cond)) LOG(kLogFatal, << "DCHECK " #cond " failed!") else;
+
+#define FATAL_CHECK(cond) \
+  do{ if (!(cond)){LOG(kLogFatal, << "FATAL_CHECK " #cond " failed!");exit(-1);}} while(0)
+
+#define LOG_ERRNO(msg) \
+  LOG(kLogFatal, << #msg " " << strerror(errno))
+
+#define RETURN_ERRNO(err) \
+  do { \
+    errno = err;\
+    return -1;\
+  } while (false)
 
 #endif // BOOST_BASE_UTILS_H
