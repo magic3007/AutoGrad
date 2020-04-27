f07341c093d57b2dd8c6a4be47a6605f5c4ec4e0
magic3007 <maijing3007@qq.com>
2020-04-27 22:11:15 +0800

    [.gitignore] ignore binary files

--- build/project1/CMakeFiles/clean_run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean_run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/clean_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/project1/CMakeFiles/solution_run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/solution_run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/solution_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
