c233b930dab604e6335163bbfa966a4c85e8895b
magic3007 <maijing3007@qq.com>
2020-04-29 00:25:53 +0800

    [solution] parse json files

--- project1/solutions/solution.cc
+++ project1/solutions/solution.cc
@@ -1,12 +1,18 @@
-#include <cstdio>
 #include <iostream>
+#include <fstream>
 #include <list>
 #include <string>
 #include <tuple>
+#include "nlohmann/json.hpp"
+#include "parser.h"
+#include "CPPPrinter.h"
+
+using namespace Boost::Internal;
 
 using std::string;
 using std::list;
 using std::tuple;
+using json = nlohmann::json;
 
 #define LIST_OF_TESTCASES \
   X(example) \
@@ -26,7 +32,21 @@ int main(int argc, char *argv[]){
   for(auto &x : testcases){
     auto input_file = std::get<0>(x);
     auto output_file = std::get<1>(x);
-    std::cout<< input_file << " " << output_file << std::endl;
+    std::ifstream in_stream(input_file);
+    if (!in_stream.is_open()){
+      std::cerr << "Could not open file " << input_file << std::endl;
+      exit(-1);
+    }
+    json j;
+    in_stream >> j;
+    in_stream.close();
+    auto name = j["name"].get<string>();
+    auto text = j["kernel"].get<string>();
+    fprintf(stdout, "=========================================\n");
+    fprintf(stdout, "%s: %s\n", name.c_str(), text.c_str());
+    Group kernel = parser::ParseFromString(text, 0);
+    CPPPrinter printer;
+    std::cout << printer.print(kernel);
   }
   return 0;
 }
